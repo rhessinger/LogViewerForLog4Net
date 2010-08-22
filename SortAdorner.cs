6cb27bda9c6231c73081ebba9893c974af7334e9
odahan_cp <SND\odahan_cp>
2010-08-22 05:32:03 +0000

    job in progress / not for distribution

---
+++ SortAdorner.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+
+namespace LogViewer
+{
+    public class SortAdorner : Adorner
+    {
+        private readonly static Geometry ascGeometry =
+            Geometry.Parse("M 0,0 L 10,0 L 5,5 Z");
+
+        private readonly static Geometry descGeometry =
+            Geometry.Parse("M 0,5 L 10,5 L 5,0 Z");
+
+        public ListSortDirection Direction { get; private set; }
+
+        public SortAdorner(UIElement element, ListSortDirection dir)
+            : base(element)
+        { Direction = dir; }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            if (AdornedElement.RenderSize.Width < 20)
+                return;
+
+            drawingContext.PushTransform(
+                new TranslateTransform(
+                  AdornedElement.RenderSize.Width - 15,
+                  (AdornedElement.RenderSize.Height - 5) / 2));
+
+            drawingContext.DrawGeometry(Brushes.Red, null,
+                Direction == ListSortDirection.Ascending ?
+                  ascGeometry : descGeometry);
+
+            drawingContext.Pop();
+        }
+    }
+}
